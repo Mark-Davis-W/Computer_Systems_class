e1aa7401bf4afc6683403bafdf467d471a4d3504
Mark-Davis-W <1874026+Mark-Davis-W@users.noreply.github.com>
2020-02-26 22:03:41 +0000

    Ctarget done!

---
+++ target84/example.d
@@ -0,0 +1,10 @@
+
+example.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	68 e5 15 40 00       	pushq  $0x4015e5
+   5:	48 c7 c7 38 d6 60 55 	mov    $0x5560d638,%rdi
+   c:	c3                   	retq   
---
+++ target84/example.s
@@ -0,0 +1,3 @@
+pushq  $0x4015e5
+movq    $0x5560d638,%rdi
+ret
