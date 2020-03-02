c0763bf394a3e230659dff405c86256b746228e5
Mark-Davis-W <1874026+Mark-Davis-W@users.noreply.github.com>
2020-03-02 03:57:05 +0000

    phase4 done!

--- target84/example.d
+++ target84/example.d
@@ -5,6 +5,6 @@ example.o:     file format elf64-x86-64
 Disassembly of section .text:
 
 0000000000000000 <.text>:
-   0:	68 e5 15 40 00       	pushq  $0x4015e5
-   5:	48 c7 c7 38 d6 60 55 	mov    $0x5560d638,%rdi
+   0:	68 97 16 40 00       	pushq  $0x401697
+   5:	48 c7 c7 73 01 96 78 	mov    $0x78960173,%rdi
    c:	c3                   	retq   
